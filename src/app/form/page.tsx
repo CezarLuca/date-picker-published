e7601b486bfaffa0912d2e5019fbb6417d73be8b
Cezar Luca <cezar.luca@hotmail.com>
2024-10-31 15:15:09 +0100

    Suspense Fallback Layer implemented

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,25 +1,10 @@
-"use client";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import Form from "../components/Form";
+import { Suspense } from "react";
+import FormPageWithoutSuspense from "./layout";
 
-const FormPage: React.FC = () => {
-    const searchParams = useSearchParams();
-    const date = searchParams.get("date");
-    const [formData, setFormData] = useState({ date: "" });
-
-    useEffect(() => {
-        if (date) {
-            setFormData({ date });
-        }
-    }, [date]);
-
-    return (
-        <div className="bg-gray-800 p-4 rounded">
-            <h1>Fill up the form to schedule an event</h1>
-            <Form formData={formData} />
-        </div>
-    );
-};
+const FormPage: React.FC = () => (
+    <Suspense fallback={<div>Loading...</div>}>
+        <FormPageWithoutSuspense />
+    </Suspense>
+);
 
 export default FormPage;
