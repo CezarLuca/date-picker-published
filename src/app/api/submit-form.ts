d0eaaed11181a29762d858ed2a38b8986f211d40
Cezar Luca <cezar.luca@hotmail.com>
2024-11-05 00:42:31 +0100

    Set up secure form submission

--- src/app/api/submit-form.ts
+++ src/app/api/submit-form.ts
@@ -1,16 +1,55 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { supabase } from "@/utils/supabaseClient";
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     if (req.method === "POST") {
-        // const { fomrData } = req.body;
+        const { date, name, email, description, captchaData } = req.body;
 
-        // Perform server-side validation and handle form submission logic
-        // For example, send the form data to your database
+        if (!name || !email || !description || !captchaData) {
+            return res.status(400).json({ error: "Please fill in all fields" });
+        }
 
-        return res.status(200).json({ success: true });
+        // Validate captcha data
+        const { selectedKeyword, question } = captchaData || {};
+        if (!selectedKeyword || !question) {
+            return res
+                .status(400)
+                .json({ error: "Please complete the captcha" });
+        }
+        if (selectedKeyword !== question) {
+            return res
+                .status(400)
+                .json({ error: "Incorrect selection, please try again." });
+        }
+
+        // Insert form data into the database
+
+        try {
+            const { error } = await supabase.from("events").insert([
+                {
+                    date,
+                    name,
+                    email,
+                    description,
+                },
+            ]);
+
+            if (error) {
+                return res
+                    .status(500)
+                    .json({ error: `Error saving data: ${error.message}` });
+            }
+
+            return res.status(200).json({ success: true });
+        } catch (err) {
+            console.error("Error saving data:", err);
+            return res
+                .status(500)
+                .json({ error: "An unexpected error occurred" });
+        }
     }
 
     res.setHeader("Allow", ["POST"]);
