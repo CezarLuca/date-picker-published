9d35680e1a58b46b0a374fb2845ee1abd1dc07be
Cezar Luca <cezar.luca@hotmail.com>
2024-11-14 22:31:31 +0100

    Updated form layout and page

--- src/app/form/layout.tsx
+++ src/app/form/layout.tsx
@@ -1,40 +1,9 @@
-"use client";
-import { useRouter, useSearchParams } from "next/navigation";
-import { Suspense, useEffect, useState } from "react";
-import Form from "../../components/Form";
-import { useAdmin } from "@/contexts/AdminContext";
+import { ReactNode } from "react";
 
-const FormPageWithoutSuspense: React.FC = () => {
-    const searchParams = useSearchParams();
-    const date = searchParams.get("date");
-    const [formData, setFormData] = useState({ date: "" });
-    const { isAdmin } = useAdmin();
-    const router = useRouter();
+interface LayoutProps {
+    children: ReactNode;
+}
 
-    useEffect(() => {
-        if (isAdmin) {
-            router.push("/admin/dashboard");
-        }
-    }, [isAdmin, router]);
-
-    useEffect(() => {
-        if (date) {
-            setFormData({ date });
-        }
-    }, [date]);
-
-    return (
-        <div className="bg-gray-800 p-4 rounded">
-            <h1>Fill up the form to schedule an event</h1>
-            <Form formData={formData} />
-        </div>
-    );
-};
-
-const FormPageWithSuspense: React.FC = () => (
-    <Suspense fallback={<div>Loading...</div>}>
-        <FormPageWithoutSuspense />
-    </Suspense>
-);
-
-export default FormPageWithSuspense;
+export default function Layout({ children }: LayoutProps) {
+    return <div className="bg-gray-800 p-4 rounded">{children}</div>;
+}
--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,10 +1,32 @@
-import { Suspense } from "react";
-import FormPageWithoutSuspense from "./layout";
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense, useEffect, useState } from "react";
+import Form from "../../components/Form";
+import { useAdmin } from "@/contexts/AdminContext";
 
-const FormPage: React.FC = () => (
-    <Suspense fallback={<div>Loading...</div>}>
-        <FormPageWithoutSuspense />
-    </Suspense>
-);
+export default function FormPage() {
+    const searchParams = useSearchParams();
+    const date = searchParams.get("date");
+    const [formData, setFormData] = useState({ date: "" });
+    const { isAdmin } = useAdmin();
+    const router = useRouter();
 
-export default FormPage;
+    useEffect(() => {
+        if (isAdmin) {
+            router.push("/admin/dashboard");
+        }
+    }, [isAdmin, router]);
+
+    useEffect(() => {
+        if (date) {
+            setFormData({ date });
+        }
+    }, [date]);
+
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <h1>Fill up the form to schedule an event</h1>
+            <Form formData={formData} />
+        </Suspense>
+    );
+}
